20cca09fb78974c3adaea83b6cf7c9b91782c688
takumi1 <avastr@yandex.ru>
2022-04-24 19:42:08 +0700

    Готовое домашнее задание на классовом компоненте.

---
+++ src/ShopItemClass.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css'
+
+class ShopItemClass extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        const {item} = this.props
+        return (
+            <div className="main-content">
+                <h2>{item.brand}</h2>
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <div className="description">
+                    {item.descriptionFull}
+                </div>
+                <div className="highlight-window mobile">
+                    <div className="highlight-overlay"></div>
+                </div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                    <div className="price">
+                        {item.currency}
+                        {`${item.price},00`}
+                    </div>
+                    <button>Добавить в корзину</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+ShopItemClass.propTypes = {
+    item: PropTypes.object
+}
+export default ShopItemClass;
